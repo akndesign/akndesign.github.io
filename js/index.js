79488fa87c5bca729a5de463b16ac6c7d334f18b
ALEXANDER <ALEXANDERKOZIOL@HOTMAIL.COM>
2020-05-22 03:41:54 +0100

    big commit for the design slider.. finally is working as expected, however some minor styling with the arrows as well as the general polish.

--- js/index.js
+++ js/index.js
@@ -1,122 +1,95 @@
-(function($) {
-  "use strict"; // Start of use strict
+var app = {};
 
+app.smoothScrolling = function() {
 
-// stop playing the youtube video when I close the modal
-/*$('#myModal').on('hide.bs.modal', function (e) {
-    $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
-});
-
-var vidUrl = "https://player.vimeo.com/video/224464481?&title=0&byline=0&portrait=0badge=0";
-
-    function embedVideo(video) {
-  var div = document.getElementById('vimeo');
-  div.innerHTML = unescape(video.html);
-
-  var ifr = div.firstChild;
-  ifr.addEventListener('load', function(e) {
-    var player = $f(ifr);
-    player.api('setVolume', 0);
-  });
-}
-
-function init() {
-  var js = document.createElement('script');
-  js.setAttribute('type', 'text/javascript');
-  js.setAttribute('src', url);
-  document.getElementsByTagName('head').item(0).appendChild(js);
-}
-
-    //Basically stops and starts the video on modal open/close
-    $('#myModal').on('hidden.bs.modal', function (e) {
-      console.log('working too');
-        $("#PlayerID iframe").attr('src','');
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
     });
+};
 
-    $('#myModal').on('show.bs.modal', function (e) {
-            console.log('working');
-      $(this).find('iframe').attr('src', vidUrl);
-      /*
-        $("PlayerID iframe").attr('src =', vidUrl);
-    });*/
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
- var images = ['logoicons_01.svg', 'logoicons_02.svg', 'logoicons_03.svg', 'logoicons_04.svg'];
+// Closes responsive menu when a scroll trigger link is clicked
     
-//Build the img, then do a bit of maths to randomize load and append to a div. Add a touch off css to fade them badboys in all sexy like.
+app.randomisedLogos = function() {
+
+    var images = ['logoicons_01.svg', 'logoicons_02.svg', 'logoicons_03.svg', 'logoicons_04.svg'];
+
+    //Build the img, then do a bit of maths to randomize load and append to a div. Add a touch off css to fade them badboys in all sexy like.
     $('<img style="width:2.5em;" src="img/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('.navbar-brand');
     $('<img style="width:2.5em; margin-bottom:10px;" src="img/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('.splash-logo');
     $('<img style="width:1.5em; margin-bottom:10px;" src="img/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('.manifesto-logo');
 
+$('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+};
+    
+app.fadeEffectHeroImages = function() {
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-$(window).scroll(function() {
-  var scrollTop = $(this).scrollTop();
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
 
 
-if (window.matchMedia("(max-width: 732px)").matches) { 
+        if (window.matchMedia("(max-width: 732px)").matches) {
 
-  $('.features').css({
-  opacity: function() {
-    var elementHeight = $(this).height(),
-   
-    opacity = ((1 - (elementHeight - scrollTop) / elementHeight)-0.15);
+            $('.features').css({
+                opacity: function() {
+                    var elementHeight = $(this).height(),
 
-    //console.log('Element ' + elementHeight, 'Scroll Top' + scrollTop);
-    return opacity;
-  }
-});
-}
-else
-{ 
-  $('.features').css({
-  opacity: function() {
-    var elementHeight = $(this).height(),
-   
-    opacity = ((1 - (elementHeight - scrollTop) / elementHeight) - 2.15);
+                        opacity = ((1 - (elementHeight - scrollTop) / elementHeight) - 0.15);
 
-    //console.log('Element ' + elementHeight, 'Scroll Top' + scrollTop);
-    return opacity;
-  }
-});
+                    //console.log('Element ' + elementHeight, 'Scroll Top' + scrollTop);
+                    return opacity;
+                }
+            });
+        } else {
+            $('.features').css({
+                opacity: function() {
+                    var elementHeight = $(this).height(),
 
-}
+                        opacity = ((1 - (elementHeight - scrollTop) / elementHeight) - 2.15);
 
-$(document).ready(function() {
- 
- function dxSimpleSlider(element = '#dx-slider', auto = false, pause) {
+                    //console.log('Element ' + elementHeight, 'Scroll Top' + scrollTop);
+                    return opacity;
+                }
+            });
+        }
+    });
+  };
+
+app.designSlider = function() {
+
+    $('#passionforDesign').click(function() {
+        $('#designGalleryStart').addClass('is-hidden');
+        $('#designFeatureOne').removeClass('is-hidden').fadeIn('slow');
+        $('#passionforDesign').removeClass('is-hidden').fadeOut('slow');
+
+    });
+
+    function designSimpleSlider(element = '#dx-slider', auto = false, pause) {
 
         // Get parent element
         var $this = $(element);
 
         // Slides container
-        var slidesCont = $this.children('.slides-container');
-        // Get all slides
-        var slides = slidesCont.children('.slide');
+        var slidesCont = $('.slides-container');
+        var slidesDotsCont = $('.slides-dot-container');
 
-        // Get Dots
-        var slidesDotsCont = $this.children('.sliderDotDiv');
-        // Get all slides
+        var arrowsCont = $('.arrows');
+      
+        var slides = slidesCont.children('.slide');
         var slidesDots = slidesDotsCont.children('#sliderDot');
-        console.log(slidesDots);
-        // Get Previous / Next links
-        var arrowsCont = $this.children('.arrows');
+       
+      
         var prevSlide = arrowsCont.children('.sliderPrev');
         var nextSlide = arrowsCont.children('.sliderNext');
 
@@ -126,6 +99,8 @@ $(document).ready(function() {
         // Set currentSlide to first child
         var currentSlide = slides.first();
         var currentDot = slidesDots.first();
+      
+        
         var currentSlideIndex = 1;
         //var sliderDots = slides.first();
 
@@ -136,93 +111,98 @@ $(document).ready(function() {
         // Function responsible for fading to next slide
         function fadeNext() {
             currentSlide.removeClass('active').fadeOut(700);
-            currentDot.removeClass('active');
+            ////currentText.removeClass('active');
+            currentDot.removeClass('dot-active');
 
-            if(currentSlideIndex == slidesCount) {
+            if (currentSlideIndex === slidesCount) {
                 currentSlide = slides.first();
                 currentDot = slidesDots.first();
-                currentSlide.delay(500).addClass('active').fadeIn(700);
-                currentDot.addClass('active');
                 currentSlideIndex = 1;
+                
+                currentSlide.delay(500).addClass('active').fadeIn(700); 
+                currentDot.addClass('dot-active');
+                
 
             } else {
                 currentSlideIndex++;
                 currentSlide = currentSlide.next();
                 currentDot = currentDot.next();
+                console.log("working:");
+
                 currentSlide.delay(500).addClass('active').fadeIn(700);
-                currentDot.addClass('active');
-                
+                currentDot.addClass('dot-active');
             }
-            
+
         }
 
         // Function responsible for fading to previous slide
         function fadePrev() {
             currentSlide.removeClass('active').fadeOut(700);
-            currentDot.removeClass('active');
+            currentDot.removeClass('dot-active');
+            
 
-            if(currentSlideIndex == 1) {
+            if (currentSlideIndex == 1) {
                 currentSlide = slides.last();
                 currentDot = slidesDots.last();
                 currentSlide.delay(500).addClass('active').fadeIn();
                 currentDot.addClass('active');
                 currentSlideIndex = slidesCount;
-                
+
             } else {
                 currentSlideIndex--;
                 currentSlide = currentSlide.prev();
                 currentDot = currentDot.prev();
                 currentSlide.delay(500).addClass('active').fadeIn(700);
-                currentDot.addClass('active');
+                currentDot.addClass('dot-active');
             }
         }
-/*
-        function Dots () {
-          if(currentSlideIndex === 1) {
-          $('.dot1').addClass('active');
-          $('.dot3').removeClass('active');
-          $('.dot2').removeClass('active');
-        
-        } else if(currentSlideIndex === 2) {
-          $('.dot2').addClass('active');
-          $('.dot3').removeClass('active');
-          $('.dot1').removeClass('active');
-          }
-        else if(currentSlideIndex === 3) {
-          $('.dot3').addClass('active');
-          $('.dot2').removeClass('active');
-          $('.dot1').removeClass('active');
-        }
-        }
-*/
-      
+       
         // Detect if user clicked on arrow for next slide and fade next slide if it did
         $(nextSlide).click(function(e) {
             e.preventDefault();
             fadeNext();
-            //Dots();
-            
+           
+
         });
 
         // Detect if user clicked on arrow for previous slide and fade previous slide if it did
         $(prevSlide).click(function(e) {
             e.preventDefault();
             fadePrev();
-            //Dots();
         });
 
-      
+
 
     }
 
-$(function() {
-        dxSimpleSlider('#slider', 8000);
+    $(function() {
+        designSimpleSlider('#slider', 8000);
     });
 
-  });
+};
 
-    
+app.navBar = function() {
 
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+};
 
 $(window).scroll(function() {
     var height = $(window).scrollTop();
@@ -235,37 +215,21 @@ $(window).scroll(function() {
 $(document).ready(function() {
     $("#back2Top").click(function(event) {
         event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
+        $("html, body").animate({
+            scrollTop: 0
+        }, "slow");
         return false;
     });
 
 });
 
-$('#passionforDesign').click(function() { 
-        $('#designGalleryStart').addClass('is-hidden');
-        $('#designFeatureOne').removeClass('is-hidden').fadeIn('slow');
-        $('#passionforDesign').removeClass('is-hidden').fadeOut('slow');
-
-    }); 
-});
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+app.init = function() {
+    app.navBar();
+    app.smoothScrolling();
+    app.randomisedLogos();
+    app.fadeEffectHeroImages();
+    app.designSlider();
     
-})(jQuery); // End of use strict
+};
+
+$(document).ready(app.init);
